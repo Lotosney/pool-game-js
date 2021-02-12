2c5cb6aa0d97396cfd39bb1a44cc5fc25eaba265
Lotosney <sebastiandwojcik@gmail.com>
2021-02-12 20:06:41 +0100

    Add ball shooting functionality

--- Stick.js
+++ Stick.js
@@ -1,17 +1,27 @@
 const STICK_ORIGIN = new Vector2(970,11)
-
-function Stick(position){
+const STICK_SHOT_ORIGIN = new Vector2(950,11)
+function Stick(position,onShoot){
     this.position= position;
     this.rotation= 0;
+    this.origin=STICK_ORIGIN.copy();
+    this.power=0;
+    this.onShoot =onShoot;
 
 
 }
 Stick.prototype.update = function(){
+
+    if (Mouse.left.down){
+        this.increasePower();
+    }
+    else if (this.power>0){
+        this.shoot();
+    }
   this.updateRotation();
 
 }
 Stick.prototype.draw=function(){
-    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN, this.rotation)
+    Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation)
 }
 
 Stick.prototype.updateRotation = function(){
@@ -19,4 +29,13 @@ Stick.prototype.updateRotation = function(){
     let adjacent = Mouse.position.x -this.position.x;
 
     this.rotation = Math.atan2(opposite,adjacent)
+}
+Stick.prototype.increasePower = function(){
+    this.power+=100;
+    this.origin.x+=5;
+}
+Stick.prototype.shoot = function(){
+    this.onShoot(this.power, this.rotation);
+    this.power = 0;
+    this.origin =STICK_SHOT_ORIGIN.copy();
 }
